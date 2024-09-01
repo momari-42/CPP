e582df1fefa07a4ce4bafee663b759df2b648d11
Mohammed Omari <momari@e3r6p3.1337.ma>
2024-09-01 16:44:17 +0100

    .

--- CPP_Module_01/ex04/ReplaceFile.cpp
+++ CPP_Module_01/ex04/ReplaceFile.cpp
@@ -6,7 +6,7 @@
 /*   By: momari <momari@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/01 14:14:42 by momari            #+#    #+#             */
-/*   Updated: 2024/09/01 15:53:43 by momari           ###   ########.fr       */
+/*   Updated: 2024/09/01 16:43:33 by momari           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,35 +22,55 @@ void readAllLine(std::istream& inFile, std::string &fileContent)
 }
 
 
-static void openFile(char *fileName, std::string &fileContent)
+static void openInFile(char *fileName, std::string &fileContent)
 {
     std::string line;
 
     std::ifstream inFile(fileName);
     if (!inFile.is_open())
     {
-        std::cout << "Error: opening file ";
+        std::cout << "Error: opening infile ";
         std::cout << fileName;
         std::cout << std::endl;
         exit (1);
     }
     readAllLine(inFile, fileContent);
+    inFile.close();
 }
 
 void findAndReplace(std::string &fileContent, std::string s1, std::string s2)
 {
     size_t i = 0;
+    if (s1.empty())
+        return ;
+    std::cout << "test" << std::endl;
     while ((i = fileContent.find(s1, i)) != std::string::npos) {
-        fileContent.replace(i, s1.length(), s2);
+        fileContent.erase(i, s1.length());
+        fileContent.insert(i, s2);
         i += s2.length();
     }
+}
+
+void openOutFile(std::string fileContent, std::string fileName)
+{
+    (void) fileContent;
+    std::string outFileName = fileName + ".replace";
+    std::fstream outFile(outFileName, std::ios::out | std::ios::app);
+    if (!outFile.is_open())
+    {
+        std::cout << "Error: opening outfile ";
+        std::cout << outFileName;
+        std::cout << std::endl;
+        exit (1);
+    }
+    
 }
 
 void replaceFile(char **av)
 {
     std::string fileContent;
 
-    openFile(av[1], fileContent);
+    openInFile(av[1], fileContent);
     findAndReplace(fileContent, av[2], av[3]);
-    std::cout << fileContent << std::endl;
+    openOutFile(fileContent, av[1]);
 }
